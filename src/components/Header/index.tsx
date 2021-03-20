c6884fea9835688576a10be1fe08240e65cb9b93
Leonardo <leonardo.brasil59@outlook.com>
2021-03-20 12:14:58 -0300

    :rocket: Added Modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,12 +3,16 @@ import logoImg from '../../assets/logo.svg';
 
 import { Container, Content } from './styles';
 
-export const Header: React.FC = () => {
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
+
+export const Header: React.FC<HeaderProps> = ({onOpenNewTransactionModal }: HeaderProps) => {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt=""/>
-        <button type="button"> Nova transação</button>
+        <button type="button" onClick={onOpenNewTransactionModal}> Nova transação</button>
       </Content>
     </Container>
   )
