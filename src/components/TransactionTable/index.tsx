4fb02ee693212dd5f59cdb544e02c29a569d883d
Leonardo <leonardo.brasil59@outlook.com>
2021-03-20 11:43:26 -0300

    :rocket: Added api mocked

--- src/components/TransactionTable/index.tsx
+++ src/components/TransactionTable/index.tsx
@@ -1,39 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { api } from '../../services/api';
 import { Container } from './styles';
 
+
+
+function Table() {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Titulo</th>
+          <th>Valor</th>
+          <th>Categoria</th>
+          <th>Data</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr>
+          <td>Desenvolvimento</td>
+          <td>R$120</td>
+          <td>Desenvolvimento</td>
+          <td>20/02/2021</td>
+        </tr>
+      </tbody>
+    </table>
+  )
+}
+
 export const TransactionsTable = () => {
+
+  useEffect(() => {
+    api.get('transactions')
+      .then(response => console.log(response))
+  }, [])
+
   return (
     <Container>
-      <table>
-        <thead>
-          <tr>
-            <th>Titulo</th>
-            <th>Valor</th>
-            <th>Categoria</th>
-            <th>Data</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          <tr>
-            <td>Desenvolvimento</td>
-            <td>R$120</td>
-            <td>Desenvolvimento</td>
-            <td>20/02/2021</td>
-          </tr>
-          <tr>
-            <td>Desenvolvimento</td>
-            <td>R$120</td>
-            <td>Desenvolvimento</td>
-            <td>20/02/2021</td>
-          </tr><tr>
-            <td>Desenvolvimento</td>
-            <td>R$120</td>
-            <td>Desenvolvimento</td>
-            <td>20/02/2021</td>
-          </tr>
-        </tbody>
-      </table>
+      <Table />
     </Container>
   )
 }
